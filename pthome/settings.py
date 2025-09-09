f3673941acd2c2b849fe3796a5eed4d6eca1b27c
alexszeliga <alexszeliga@gmail.com>
2025-09-09 22:17:27 +0000

    feat: use redis cache

--- pthome/settings.py
+++ pthome/settings.py
@@ -41,7 +41,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_redis',
 ]
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
