5cb6f8ed67251807cd4dc5feb687a89f1f08e5b7
alexszeliga <alexszeliga@gmail.com>
2025-09-26 12:08:23 +0000

    add septa route model and command to populate from gtfs data

--- septa/models.py
+++ septa/models.py
@@ -9,3 +9,10 @@ class SeptaLocation(locationModels.Location):
     
     def __str__(self):
         return self.location_name
+
+class Route(models.Model):
+    route_id = models.CharField(max_length=255, unique=True,db_index=True)
+    route_short_name = models.CharField(max_length=255)
+    route_long_name = models.CharField(max_length=255)
+    route_desc = models.CharField(max_length=255)
+    route_type = models.CharField(max_length=255)
